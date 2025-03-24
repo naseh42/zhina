8b8b36e3c95e6b4f7b512d400f4a0b91eaebcd94
naseh42 <naseh42@gmail.com>
2025-03-24 23:25:45 +0330

    Update __init__.py

--- backend/users/__init__.py
+++ backend/users/__init__.py
@@ -1,6 +1,6 @@
-create_user.py
-delete_user.py
-edit_user.py
-user_page.py
-user_stats.py
-user_subscription.py
+from .create_user import *
+from .delete_user import *
+from .edit_user import *
+from .user_page import *
+from .user_stats import *
+from .user_subscription import *
