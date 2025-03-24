dd689692dd26a6b076a1bc58c3c662e68153eea3
naseh42 <naseh42@gmail.com>
2025-03-24 23:22:57 +0330

    Update __init__.py

--- backend/domains/__init__.py
+++ backend/domains/__init__.py
@@ -1,6 +1,6 @@
-add_domain.py
-cdn_management.py
-delete_domain.py
-domain_config.py
-domain_subscription.py
-edit_domain.py
+from .add_domain import *
+from .cdn_management import *
+from .delete_domain import *
+from .domain_config import *
+from .domain_subscription import *
+from .edit_domain import *
