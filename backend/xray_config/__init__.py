fd6a864a8349f098f86b676082b300232df0d03c
naseh42 <naseh42@gmail.com>
2025-03-25 19:54:27 +0330

    Update __init__.py

--- backend/xray_config/__init__.py
+++ backend/xray_config/__init__.py
@@ -1,4 +1,20 @@
+from .settings import xray_settings
+from .tls_http import http_settings, tls_settings
 from .inbounds import *
 from .protocols import *
 from .subscription import *
-from .tls_http import *
+
+__all__ = [
+    'xray_settings',
+    'http_settings',
+    'tls_settings',
+    # توابع و کلاس‌های export شده از ماژول‌های دیگر
+    'InboundCreate',
+    'InboundUpdate',
+    'ProtocolType',
+    'SubscriptionCreate',
+    'SubscriptionUpdate',
+    'create_subscription',
+    'update_subscription',
+    'delete_subscription'
+]
