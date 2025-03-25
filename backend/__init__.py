9ed69e9d52f9195b7cd7c82de53a54fdc0cdcdc9
naseh42 <naseh42@gmail.com>
2025-03-25 21:53:27 +0330

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
 from .app import app
-from .config import Config
+from .config import settings  # استفاده از آبجکت settings که در config.py تعریف شده
 from .database import Base, engine, SessionLocal
 from .models import *
 from .schemas import *
