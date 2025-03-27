cea1b5424b9344818fa399f0cab75d8f8bef4edf
naseh42 <naseh42@gmail.com>
2025-03-27 17:11:24 +0330

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,10 +14,10 @@ import sys
 sys.path.append(str(Path(__file__).parent.parent))
 
 # Importهای داخلی
-from backend . import schemas, models, utils
-from backend .database import get_db, engine, Base
-from backend .config import settings
-from backend .xray_config import xray_settings
+from backend import schemas, models, utils
+from backend.database import get_db, engine, Base
+from backend.config import settings
+from backend.xray_config import xray_settings
 
 # Initialize FastAPI
 app = FastAPI(
