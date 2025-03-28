d70578df2454e5eceba4b3a7e81f3817083e76cb
naseh42 <naseh42@gmail.com>
2025-03-28 23:13:40 +0330

    Update __init__.py

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -3,3 +3,17 @@ from .advanced_settings import *
 from .change_password import *
 from .language import *
 from .theme import *
+
+from .settings_manager import SettingsManager
+from .admin_manager import AdminManager
+from .domain_config import DomainConfigManager
+from .appearance import AppearanceSettings
+from .security import SecurityManager
+
+__all__ = [
+    'SettingsManager',
+    'AdminManager',
+    'DomainConfigManager',
+    'AppearanceSettings',
+    'SecurityManager'
+]
