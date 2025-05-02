e3baf809226effc799a3075a6be715b0e1dd4ee1
naseh42 <naseh42@gmail.com>
2025-05-02 12:33:45 +0330

    Update __init__.py

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,1 +1,9 @@
-from .xray import *
+from .xray import router as xray_router
+from .domain_router import router as domain_router
+from .user_routes import router as user_router
+
+__all__ = [
+    "xray_router",
+    "domain_router",
+    "user_router",
+]
